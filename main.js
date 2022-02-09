db1da4eab5c56a5d332b0d4a1f2871a71bf50143
zihuns <seojh44@uos.ac.kr>
2022-02-10 02:40:05 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,12 @@
+let str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+const regexp = /HEROPY/gi
+// console.log(regexp.test(str))
+str = str.replace(regexp, 'AAA')
+console.log(str)
